7803e859fbbc91b27a0fb4ff9d399646816d40c6
Dan OBoyle <oboyledk@gmail.com>
2018-07-24 23:02:14 -0400

    change extention to lua

---
+++ Rogue Macros.lua
@@ -0,0 +1,41 @@
+
+#showicon
+#showtooltip
+/assist
+/cast  Tricks of the Trade
+/assist
+
+
+#showtooltip
+#showicon
+/cast [mod:shift,stance] Distract; [stance] Pick Pocket; Mutilate
+
+
+
+#showtooltip
+#showicon
+/cast [mod:shift,stance] Garrote; [mod:alt,stance] Distract; [stance] Cheap Shot; [mod:shift] Kick; [mod:alt] sprint; Mutilate
+
+
+#showtooltip
+#showicon
+/cast [mod:alt] Crimson Vial; Fan of Knives
+
+#showtooltip
+#showicon
+/cast [mod:shift] Cloak of Shadows;[mod:alt] Shadowmeld; Vanish
+
+
+#showtooltip
+#showicon
+/cast [mod:alt] Stealth; [mod:shift] Rupture; Envenom
+
+
+#showtooltip
+#showicon
+/cast [mod:shift] Blind; [mod:alt] sprint; Shadowstep
+
+
+#showicon
+#showtooltip
+/cast [target=mouseover, harm, exists, nodead] Poisoned Knife; Poisoned Knife
